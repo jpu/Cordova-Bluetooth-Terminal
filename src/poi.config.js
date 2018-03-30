a28dc975431b3980f52d07b6f9ec177774f332d8
Juha Pulliainen <juha.pulliainen@bitville.fi>
2018-03-30 09:36:44 +0300

    Update sockjs patch instructions in poi.config.js

--- src/poi.config.js
+++ src/poi.config.js
@@ -10,15 +10,17 @@ module.exports = (options, req) => ({
         template: 'index.ejs'
     },
     webpack(config) {
-        // App js files on device
+        // App js files on device:
         //config.output.publicPath = "";
-        // App js files on dev server (with hot reload)
+        // App js files on dev server (with hot reload):
         config.output.publicPath = "http://192.168.8.115:4000/";
         config.resolve.alias['vue$'] = 'vue/dist/vue.esm.js';
-        // Note: To make sockjs client work using a dev server
+        // Note: To make sockjs client work using a dev server, with
+        // your phone and your computer on the same local network,
         // you'll need to patch sockjs.js:
         // Add the line
         //   url = (url == "/sockjs-node") ? "http://192.168.1.253:4000/sockjs-node" : url;
+        // (replace 192.168.1.253:4000 with the ip & port of your computer)
         // before the line
         //   var parsedUrl = new URL(url);
         
